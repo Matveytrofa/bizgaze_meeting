64dd7c89a685ce98312594726dbf46879e3376cb
Matveytrofa <trofik-motya@mail.ru>
2021-08-31 12:40:47 +0900

    fix

---
+++ videoconf/scripts/protocol/jitsi.ts
@@ -0,0 +1,29 @@
+﻿
+/* SHOULD not exist same value in two enums
+ */
+
+
+export enum JitsiCommand {
+    GRANT_HOST_ROLE = "grant-host",
+    MUTE_AUDIO = "mute_audio",
+    MUTE_VIDEO = "mute_video",
+    ALLOW_CAMERA = "allow_video",
+    ALLOW_MIC = "allow_audio",
+    INIT_MEDIA_POLICY = "init_media_policy",
+    ASK_RECORDING = "ask-recording",
+    ASK_SCREENSHARE = "ask-screenshare",
+    ASK_HANDRAISE = "ask-handraise",
+    FILE_META = "file_meta",
+    FILE_SLICE = "file_slice",
+    BIZ_ID = "biz_id",
+};
+
+
+
+export enum JitsiPrivateCommand {
+    MEDIA_POLICY = "media_policy",
+    ALLOW_RECORDING = "allow_recording",
+    ALLOW_SCREENSHARE = "allow_screenshare",
+    ALLOW_HANDRAISE = "allow_handraise",
+    PRIVATE_CAHT = "private_chat"
+}
